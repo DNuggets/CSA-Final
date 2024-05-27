8fa42fd1ac07bb4b989a6b5e32e5b877cec12727
sokul_13 <stemsokul2025@gmail.com>
2024-05-26 19:21:10 -0700

    lol2
    lol2

---
+++ DataRunner.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DataRunner {
+  public static void main(String[] args) {
+    Player player1 = new Player();
+    Player player2 = new Player();
+    
+    //inputs the data of the teams into their respective objects from their data files
+    Stats teaminfo = new Stats("team.txt", "names.txt", "threePT.txt", "middy.txt", "jamtime.txt");
+    
+    //prints out the toString() method showing all the user all of the info of all of the teams
+    System.out.println(teaminfo);
+
+    //prompts the user to input the number of championships they want the team that they are advertising with to have, and outputs the amount teams that have the inputted amount of championships (method 1)
+    String userResponse1 = teaminfo.player1Prompt();
+    player1 = teaminfo.assignTeam(userResponse1);
+
+    //prompts the user to input the number of championships they want the team that they are advertising with to have, and outputs the amount teams that have the inputted amount of championships (method 1)
+    String userResponse2 = teaminfo.player2Prompt();
+    player2 = teaminfo.assignTeam(userResponse2);
+
+  }
+}
---
+++ Stats.java
@@ -0,0 +1,82 @@
+import org.code.theater.*;
+import org.code.media.*;
+import java.util.Scanner;
+
+public class Stats{
+
+  private Player[] teamInfo;    // The 1D array of all of the teams' info
+
+  /*
+   * Reads the data from teamsFile, locationsFile, arenasFile, capacitiesFile, championshipsFile, inSeasonTournamentChampionshipsFile to initialize team infos
+   */
+  public Stats(String teamsFile, String namesFile, String threeptFile, String middyFile, String jamtimeFile) {
+    teamInfo = createResponses(teamsFile, namesFile, threeptFile, middyFile, jamtimeFile);
+  }
+
+  /*
+   * Returns a 1D array of team objects using the data from teamsFile, locationsFile, arenasFile, capacitiesFile, championshipsFile, inSeasonTournamentChampionshipsFile
+   * Precondition: all strings taken in are valid file names
+   * Postcondition: must input each line of the respective file into their own index of their respective array
+   */
+  public Player[] createResponses(String teamsFile, String namesFile, String threeptFile, String middyFile, String jamtimeFile) {  
+    String[] teamsData = FileReader.toStringArray(teamsFile);
+    String[] namesData = FileReader.toStringArray(namesFile);
+    Double[] threeptData = FileReader.toDoubleArray(threeptFile);
+    Double[] middyData = FileReader.toDoubleArray(middyFile);
+    Double[] jamtimeData = FileReader.toDoubleArray(jamtimeFile);
+    
+    
+    Player[] tempResponses = new Player[teamsData.length];
+    for (int index = 0; index < tempResponses.length; index++) {
+      tempResponses[index] = new Player(teamsData[index], namesData[index], threeptData[index], middyData[index], jamtimeData[index]);
+    }
+    return tempResponses;
+  }
+  
+  /*
+   * Prompts the user to enter the amount of championships they want their perspective advertising partner team to have (for credibility of team's following)
+     * Precondition: must have Scanner class imported in file previously 
+   * Postcondition: must prompt the user with a string question, and assign the user inputted string to the choice variable
+   */
+  public String player1Prompt() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Player1!");
+    System.out.println("Choose the NBA team that you want to play as: ");
+    String choice = scanner.nextLine();
+    scanner.close();
+    return choice;
+  }
+
+  public String player2Prompt() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Player2!");
+    System.out.println("Choose the NBA team that you want to play as: ");
+    String choice = scanner.nextLine();
+    scanner.close();
+    return choice;
+  }
+
+  public Player assignTeam(String teamName) {
+    for (int i =0; i<teamInfo.length; i++) {
+      if (teamName.equals(teamInfo[i].getTeam())){
+        return teamInfo[i];
+      }
+    }
+    return null;
+  }
+
+
+  /*
+   * Returns a String containing each team's information
+   */
+  public String toString() {
+    String result = "";
+
+    for (Player answer : teamInfo) {
+      result += answer + "\n";
+    }
+
+    return result;
+  }
+  
+}
